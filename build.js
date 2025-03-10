e2703c9a59d54d32b3288ba13c72af9fc1d72c9e
nx0ly <n9d8sa@gmail.com>
2025-03-10 12:56:10 -0400

    test

--- build.js
+++ build.js
@@ -2,5 +2,6 @@ require("esbuild").build({
     entryPoints: ["server/index.ts"],
     bundle: true,
     outdir: "dist",
-    platform: "node"
+    platform: "node",
+    target: "node18",
 }).catch(() => process.exit(1));
